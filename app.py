79b205872912c6797a02142d3ee99bd264822634
Cora Wu <cjw322@cornell.edu>
2021-08-13 12:20:51 -0700

    Added more documentation

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+# -*- coding: UTF-8 -*-    
+
 from flask import Flask, request, render_template, redirect, url_for
 from flask_cors import CORS
 import jieba
@@ -21,15 +23,16 @@ def translate(text):
   tk_dict = create_tk_dict(jieba.tokenize(text,mode='search'))
   trans_dict = create_trans_dict(text, jieba.tokenize(text,mode='search'))
   seg_list = create_seg_list(text)
-  print('\n\n\n\n\nResults')
-  print('tk dict ', tk_dict)
-  print('trans dict ', trans_dict)
-  print('seg list', seg_list)
+  # print('\n\n\n\n\nResults')
+  # print('tk dict ', tk_dict)
+  # print('trans dict ', trans_dict)
+  # print('seg list', seg_list)
   return render_template(
     "index.html", 
     input=text, seg_list=seg_list, 
     tkn_dict=tk_dict, trans_dict=trans_dict
   )
+
 
 def is_chn(character):
    return character > u'\u4e00' and character < u'\u9fff'
@@ -41,8 +44,13 @@ def create_tk_dict(tk_list):
 
 	Returns a dictionary with integer keys representing the index of each valid 
   Chinese character mapped to list values of string tokens
+
+  Example:
+  Input: 大家好！
+  Output: {0: ['大家'], 1: ['大家'], 2: ['好']}
 	"""
   tk_dict = {}
+
   for tk in tk_list:
     # print("word %s\t\t start: %d \t\t end:%d" % (tk[0],tk[1],tk[2]))
     token = tk[0]
@@ -54,25 +62,34 @@ def create_tk_dict(tk_list):
           tk_dict.update({ start+i : [token] })
         else:
           tk_dict[start+i] += [token]
+    
   return tk_dict
+
 
 def create_trans_dict(text, tk_list):
   """
-  Returns a mapping of translations for all characters and tokens in tk_list
+  Returns a mapping of all characters and tokens in tk_list to a tuple of their 
+  corresponding pinyin and translation
+
+  The returned mapping looks like this: 
+  { <token> : (<pinyin>, <translation>) }
+
+  Example:
+  {'家': ('jiā', 'Home'), '大': ('dà', 'Big'), 
+  '好': ('hǎo', 'it is good'), '大家': ('dàjiā', 'Everyone')}
   """
-  print("Creating Trans Dict")
+  # print("Creating Trans Dict")
   trans_dict = {}
   tokens = {tk[0] for tk in tk_list}
   characters = {c for c in text if  is_chn(c)}
-  to_translate = tokens.union(characters)
 
+  to_translate = tokens.union(characters)
   for t in to_translate:
-    print('token ', t)
     if len(t) > 0 and is_chn(t[0]):
       if t not in trans_dict:
         py = pinyin.get(t)
         translator = Translator()
-        print('token before trans ', t)
+        # print(translator.translate(t))
         trans = translator.translate(t).text
         trans_dict.update({ t : (py, trans) })
       else:
@@ -107,6 +124,7 @@ def create_seg_list(text):
 @app.route("/")
 def home():
   return render_template("index.html", input="", seg_list=None, tkn_dict=None)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
